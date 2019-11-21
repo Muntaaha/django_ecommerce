0024818e7aa0b11e25ef4869603b778aab97850a
Muntaaha <53495294+Muntaaha@users.noreply.github.com>
2019-11-22 00:02:19 +0600

    Django Ecommerce

---
+++ orders/tasks.py
@@ -0,0 +1,19 @@
+from celery.task import task
+from django.core.mail import send_mail
+
+from orders.models import Order
+
+
+@task
+def order_created(order_id):
+    """
+    Task to send an e-mail notification when an order is successfully created.
+    :param order_id:
+    :return:
+    """
+    order = Order.objects.get(id=order_id)
+    subject = 'Order nr. {}'.format(order_id)
+    message = 'Dear {}, \n\n\You have successfully placed an order.Your order id is {}.'.format(order.first_name,
+                                                                                                order.id)
+    mail_sent = send_mail(subject, message, 'admin@myshop.com', [order.email])
+    return mail_sent
